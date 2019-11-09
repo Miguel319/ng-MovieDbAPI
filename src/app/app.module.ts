208a6800219ea460670248b3e66b1409728ac8b2
Miguel319 <miguelp90.319@gmail.com>
2019-11-09 15:18:04 -0400

    Componentes iniciales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./componentes/navbar/navbar.component";
+import { BusquedaComponent } from "./componentes/busqueda/busqueda.component";
+import { HomeComponent } from "./componentes/home/home.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BusquedaComponent,
+    HomeComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
